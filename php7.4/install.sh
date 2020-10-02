f144b5b900456fc9fc2df84ab8699ca7bef4074e
Timothy Marois <timothymarois@gmail.com>
2020-10-02 18:02:24 -0400

    Added node to install

--- php7.4/install.sh
+++ php7.4/install.sh
@@ -143,3 +143,14 @@ mkdir /usr/local/share/GeoIP
 
 # run the update command
 geoipupdate
+
+# Install NVM "nvm"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+
+# Load nvm now
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install node
+nvm install node
